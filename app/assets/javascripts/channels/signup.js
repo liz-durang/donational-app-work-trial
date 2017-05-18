3b5c3e3ce8e02dbd173d99c5628b090d2971fbdd
Ian Yamey <ian@ifthisthengive.com>
2017-05-18 12:14:13 +0200

    Ask multiple questions during signup

--- app/assets/javascripts/channels/signup.js
+++ app/assets/javascripts/channels/signup.js
@@ -1,34 +1,40 @@
 App.signup = App.cable.subscriptions.create('SignupChannel', {
   connected: function() {
+    return this.perform('start');
   },
 
   disconnected: function() {
   },
 
   received: function(data) {
+    console.log(data)
     // Called when there's incoming data on the websocket for this channel
     if (data['success'] == true) {
-      $('#conversational-form .history').append('<p style="color: blue">' + data['previous_question_html'] + '</p>')
-      $('#conversational-form .history').append('<p>' + data['previous_answer_html'] + '</p>')
-      $('#conversational-form .staging-area').html('')
-      $('#conversational-form .question').html('<p>' + data['question_html'] + '</p>')
-      $('#conversational-form .responses').html(data['responses_html'])
+      if (data['previous_question_html'] !== null) {
+        $('.history').append('<p style="color: blue">' + data['previous_question_html'] + '</p>');
+      }
+      if (data['previous_answer_html'] !== null) {
+        $('.history').append('<p>' + data['previous_answer_html'] + '</p>');
+      }
+      $('.staging .response, .staging .question').html('');
+      $('.current .question').html('<p>' + data['question_html'].join('<p></p>') + '</p>');
+      $('.current .response').html(data['responses_html']);
     }
   },
 
-  submit_answer: function(answer) {
-    $('#conversational-form .question').html('')
-    $('#conversational-form .responses').html('')
-    $('#conversational-form .staging-area').append('<p style="color: blue">What is your name?</p>')
-    $('#conversational-form .staging-area').append('<p>' + answer + '</p>')
+  submit_answer: function(answer, questionId) {
+    $('.staging .question').html($('.current .question').html())
+    $('.current .question').html('...');
+    $('.staging .response').html('<p>' + answer + '</p>');
+    $('.current .response').html('');
 
-    return this.perform('submit_question', { answer: answer });
+    return this.perform('submit_question', { answer: answer, question_id: questionId });
   }
 });
 
-$(document).on('keypress', '#conversational-form .responses', function(event) {
+$(document).on('keypress', '.current .response', function(event) {
   if (event.keyCode === 13) {
-    App.signup.submit_answer(event.target.value);
+    App.signup.submit_answer(event.target.value, $(event.target).data('questionId'));
     return event.preventDefault();
   }
 });
--- app/channels/signup_channel.rb
+++ app/channels/signup_channel.rb
@@ -3,19 +3,14 @@ class SignupChannel < ApplicationCable::Channel
 
   def subscribed
     stream_for current_donor
-
-    self.class.broadcast_to(
-      current_donor,
-      success: true,
-      previous_question_html: '',
-      previous_answer_html: '',
-      question_html: 'What is your name?',
-      responses_html: '<input type="text"/>'
-    )
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def start
+    broadcast_question(questions.first)
   end
 
   def submit_question(data)
@@ -25,16 +20,44 @@ class SignupChannel < ApplicationCable::Channel
     #   current_question_html = render_next_question
     sleep 1
 
-    self.class.broadcast_to(
-      current_donor,
-      success: true,
-      previous_question_html: 'What is your name?',
-      previous_answer_html: data['answer'],
-      question_html: 'And how old are you?',
-      responses_html: '<input type="text"/>'
+    question_id = data['question_id'].to_i
+    question = questions[question_id]
+    # response = question.save!(data['answer'])
+
+    broadcast_question(
+      questions[question_id + 1],
+      previous_answer: data['answer'],
+      previous_question: question
     )
   end
 
   # dont allow the clients to call those methods
   protected :session
+
+  private
+
+  def broadcast_question(question, previous_answer: nil, previous_question: nil)
+    self.class.broadcast_to(
+      current_donor,
+      success: true,
+      previous_question_html: previous_question.try(:[], :title),
+      previous_answer_html: previous_answer,
+      question_html: question[:title],
+      responses_html: %Q(<input type="text" data-question-id="#{question[:id]}">),
+    )
+  end
+
+  def questions
+    [
+      { id: 0, title: ['What is your name?'] },
+      { id: 1, title: ['As a percentage of annual income, how much aught individuals give to charity?'] },
+      { id: 2, title: [
+        'Donational withdraws a single monthly contribution from your account, and distributes it to your chosen charities.',
+        "We'll help you choose charities that are impactful, efficient and align with your values, but first...",
+        'time to make a commitment!',
+        'As a percentage of your annual income, how much do you want to contribute to charities?'
+        ]
+      }
+    ]
+  end
 end
