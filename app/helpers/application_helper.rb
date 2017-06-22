db7b48912ff698fc38d9ed143e7225cb52fd8829
Ian Yamey <ian@ifthisthengive.com>
2017-06-22 17:04:38 -0400

    Create home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def sign_in_button
+    link_to 'Sign in', new_sessions_path, class: 'button is-outlined'
+  end
+
+  def sign_out_button
+    link_to(
+      'Sign out',
+      sessions_path,
+      class: 'button',
+      method: :delete,
+      data: { confirm: 'Are you sure you want to sign out?' }
+    )
+  end
+
+  def sign_in_or_out_button
+    return sign_out_button if logged_in?
+    sign_in_button
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resource :sessions, only: %i[new destroy]
   get '/auth/oauth2/callback' => 'auth0#callback'
   get '/auth/failure' => 'auth0#failure'
 
   get 'dashboard' => 'dashboard#show'
 
+  root 'welcome#index'
+  
   mount ActionCable.server => '/cable'
 end
