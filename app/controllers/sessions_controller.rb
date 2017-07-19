328544a1d7860dabe52fccacf2c83f33c3085eb7
Ian Yamey <ian@ifthisthengive.com>
2017-07-19 16:16:34 -0400

    Remove inline scss

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,8 @@
 class SessionsController < ApplicationController
-  # GET /sessions/new
   def new
     redirect_to dashboard_path if logged_in?
   end
 
-  # DELETE /sessions/1
-  # DELETE /sessions/1.json
   def destroy
     log_out!
     redirect_to dashboard_path
