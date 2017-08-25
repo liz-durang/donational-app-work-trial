ef3979e19799c8c42be946f078b80dad4e4f1b85
Ian Yamey <ian@ifthisthengive.com>
2017-08-25 16:22:38 -0400

    Test features with capybara

---
+++ spec/features/visitor_signs_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Visitor signs in', type: :feature do
+  scenario 'with valid credentials', js: true do
+    visit root_path
+    click_on 'Sign in'
+    expect(page).to have_content('Auth0')
+    sign_in_as!(first_name: 'Donny', last_name: 'Donator')
+    expect(page).to have_content('Donny Donator')
+  end
+
+  def sign_in_as!(first_name:, last_name:, email: 'user@example.com')
+    OmniAuth.config.add_mock(
+      :auth0,
+      {
+        uid: '12345', info: { email: email, first_name: first_name, last_name: last_name }
+      }
+    )
+
+    visit auth_oauth2_callback_path
+  end
+end
