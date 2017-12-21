b35cb4ca155749739c29d14267ce2cb49f8cc6b8
Ian Yamey <ian@ifthisthengive.com>
2017-12-21 11:04:47 +1100

    Eliminate delay when starting onboarding

--- app/assets/javascripts/channels/onboarding.js
+++ app/assets/javascripts/channels/onboarding.js
@@ -4,82 +4,95 @@ $(document).on('click', '[data-behavior="StartOnboarding"]', function () {
   if (App.onboarding) {
     App.onboarding.start();
   } else {
-    App.onboarding = App.cable.subscriptions.create('OnboardingChannel', {
-      connected: function() {
-        this.install();
-        this.start();
-      },
+    createOnboardingChannel();
 
-      disconnected: function() { },
-
-      install: function() {
-        $(document).on('submit', 'form[data-behavior-auto-submit]', function(event) {
-          event.preventDefault();
-          const formData = $(this).serialize();
-          App.onboarding.respond(formData);
-        });
-
-        $(document).on('change', '[data-behavior-auto-submit] input[type=radio]', function(event) {
-          $(event.target.form).trigger('submit');
-        });
-      },
-
-      start: function() {
-        this.clearScreen();
-        this.perform('start');
-      },
-
-      received: function(data) {
-        if(data.redirect_to) {
-          window.location.href = data.redirect_to;
-          return;
-        }
-
-        $('#question-heading').text(data.heading);
-
-        const chatMessages = $('.chat-messages');
-
-        let cumulativeDelay = 0;
-        const readingDelay = 10;
-
-        data.messages.forEach(function(message, index, messages) {
-          if (index > 0) {
-            // Give the user time to read the previous message
-            cumulativeDelay += readingDelay * messages[index - 1].body.length;
-          }
-
-          let pendingMessage = $('<li class="chat-message typing appear-in"></li>')
-            .addClass(message.type)
-            .css({ 'animation-delay': cumulativeDelay + 'ms'})
-            .appendTo(chatMessages);
-
-          let messageText = $('<span>').text(message.body).appendTo(pendingMessage);
-
-          // Give the user time to read the current message
-          cumulativeDelay += readingDelay * message.body.length;
-
-          setTimeout(function() { pendingMessage.removeClass('typing'); }, cumulativeDelay);
-        });
-
-        $(data.responses)
-          .css({ 'animation-delay': cumulativeDelay + 500 + 'ms'})
-          .addClass('appear-in')
-          .appendTo('.chat-responses')
-          .find('input:first-of-type')
-          .focus();
-
-        window.scrollTo(0, 0);
-      },
-
-      respond: function(serializedFormData) {
-        this.clearScreen();
-        this.perform('respond', { payload: serializedFormData });
-      },
-
-      clearScreen: function() {
-        $('.chat-messages').html('');
-        $('.chat-responses').html('');
+    var waitForConnection = setInterval(function(){
+      if (App.onboarding) {
+        App.onboarding.start();
+        clearInterval(waitForConnection);
       }
-    });
+      console.log("polling");
+    }, 200);
   }
 });
+
+$(document).on('submit', 'form[data-behavior-auto-submit]', function(event) {
+  event.preventDefault();
+  const formData = $(this).serialize();
+  App.onboarding.respond(formData);
+});
+
+$(document).on('change', '[data-behavior-auto-submit] input[type=radio]', function(event) {
+  $(event.target.form).trigger('submit');
+});
+
+function createOnboardingChannel(startOnConnect) {
+  App.cable.subscriptions.create('OnboardingChannel', {
+    connected: function() {
+      App.onboarding = this;
+    },
+
+    disconnected: function() {
+      App.onboarding = null;
+    },
+
+    start: function() {
+      this.clearScreen();
+      this.perform('start');
+    },
+
+    received: function(data) {
+      if(data.redirect_to) {
+        window.location.href = data.redirect_to;
+        return;
+      }
+
+      $('#question-heading').text(data.heading);
+
+      const chatMessages = $('.chat-messages');
+
+      let cumulativeDelay = 0;
+      const readingDelay = 10;
+
+      data.messages.forEach(function(message, index, messages) {
+        if (index > 0) {
+          // Give the user time to read the previous message
+          cumulativeDelay += readingDelay * messages[index - 1].body.length;
+        }
+
+        let pendingMessage = $('<li class="chat-message typing appear-in"></li>')
+          .addClass(message.type)
+          .css({ 'animation-delay': cumulativeDelay + 'ms'})
+          .appendTo(chatMessages);
+
+        let messageText = $('<span>').text(message.body).appendTo(pendingMessage);
+
+        // Give the user time to read the current message
+        cumulativeDelay += readingDelay * message.body.length;
+
+        setTimeout(function() { pendingMessage.removeClass('typing'); }, cumulativeDelay);
+      });
+
+      $(data.responses)
+        .css({ 'animation-delay': cumulativeDelay + 500 + 'ms'})
+        .addClass('appear-in')
+        .appendTo('.chat-responses')
+        .find('input:first-of-type')
+        .focus();
+
+      window.scrollTo(0, 0);
+    },
+
+    respond: function(serializedFormData) {
+      this.clearScreen();
+      this.perform('respond', { payload: serializedFormData });
+    },
+
+    clearScreen: function() {
+      $('.chat-messages').html('');
+      $('.chat-responses').html('');
+    }
+  });
+}
+
+createOnboardingChannel();
