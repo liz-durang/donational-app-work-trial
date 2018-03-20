5eed94e3e0643093ac7b487f73595c566618c211
Ian Yamey <ian@donational.org>
2018-03-20 18:49:33 +1100

    Move logic from onboarding question into command

--- app/commands/donors/update_cause_area_relevance.rb
+++ app/commands/donors/update_cause_area_relevance.rb
@@ -1,9 +1,24 @@
 module Donors
-  class UpdateCauseAreaRelevance
-    def self.run!(donor, attributes)
-      return nil if donor.blank?
+  class UpdateCauseAreaRelevance < ApplicationCommand
+    required do
+      model :donor
+      array :causes_that_matter
+    end
 
-      CauseAreaRelevance.find_or_initialize_by(donor: donor).update!(attributes)
+    def execute
+      CauseAreaRelevance.find_or_initialize_by(donor: donor).update!(scored_cause_areas)
+    end
+
+    private
+
+    def scored_cause_areas
+      all_recommendable_causes.map do |cause_area|
+        [cause_area, cause_area.in?(causes_that_matter) ? 7 : 0]
+      end.to_h
+    end
+
+    def all_recommendable_causes
+      Organization::recommendable_cause_areas.map(&:to_sym)
     end
   end
 end
