b841a4023ca62b10e287d88a32575f329c791a58
Ian Yamey <ian@donational.org>
2018-06-30 11:19:15 -0500

    Fix message when there are no future donations

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -25,7 +25,8 @@ class ContributionsController < ApplicationController
       recurring_contribution: active_recurring_contribution,
       portfolio_organization_count: active_portfolio.active_allocations.count,
       cancel_link: 'Stop my ' + active_recurring_contribution.frequency + ' donation',
-      has_future_contribution: active_recurring_contribution.future_contribution_scheduled?
+      has_future_contribution: active_recurring_contribution.future_contribution_scheduled?,
+      next_contribution_date: active_recurring_contribution.next_contribution_at
     )
   end
 
