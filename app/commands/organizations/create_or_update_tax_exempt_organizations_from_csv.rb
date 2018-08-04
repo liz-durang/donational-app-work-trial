d820f9353b928c38973c46721ea6b4f632c3381d
Alejandro Monetti <pepo.1393@hotmail.com>
2018-08-04 13:13:21 -0300

    Rename copy method

--- app/commands/organizations/create_or_update_tax_exempt_organizations_from_csv.rb
+++ app/commands/organizations/create_or_update_tax_exempt_organizations_from_csv.rb
@@ -7,28 +7,23 @@ module Organizations
       delete_sql = "DELETE FROM searchable_organizations;"
       ActiveRecord::Base.connection.execute(delete_sql)
 
-      copy_from("db/tax_exempt_organizations/eo1.csv")
-      copy_from("db/tax_exempt_organizations/eo2.csv")
-      copy_from("db/tax_exempt_organizations/eo3.csv")
+      import_searchable_organizations_from_csv("db/tax_exempt_organizations/eo1.csv")
+      import_searchable_organizations_from_csv("db/tax_exempt_organizations/eo2.csv")
+      import_searchable_organizations_from_csv("db/tax_exempt_organizations/eo3.csv")
 
       nil
     end
 
     private
 
-    def copy_from(path)
-      io = File.open(path, 'r')
-      connection = ActiveRecord::Base.connection
-      connection.raw_connection.copy_data %{COPY searchable_organizations FROM STDIN CSV HEADER} do
-        while line = io.gets do
-          next if line.strip.size == 0
-          if block_given?
-            row = CSV.parse_line(line.strip, { col_sep: ',' })
-            yield(row)
-            next if row.all? { |f| f.nil? }
-            line = CSV.generate_line(row, { col_sep: ',' })
+    def import_searchable_organizations_from_csv(path)
+      File.open(path, 'r') do |io|
+        connection = ActiveRecord::Base.connection
+        connection.raw_connection.copy_data %{COPY searchable_organizations FROM STDIN CSV HEADER} do
+          while line = io.gets do
+            next if line.strip.size == 0
+            connection.raw_connection.put_copy_data line
           end
-          connection.raw_connection.put_copy_data line
         end
       end
     end
