45273f6d4a92f5f7b3a91253062dfd7fd5582e7b
Alejandro Monetti <pepo.1393@hotmail.com>
2018-08-06 16:12:04 -0300

    Change reminders and receipts mail origin

--- app/mailers/reminders_mailer.rb
+++ app/mailers/reminders_mailer.rb
@@ -4,6 +4,11 @@ class RemindersMailer < ApplicationMailer
     @payment_method = payment_method
     @entity = entity
 
-    mail(to: @contribution.donor.email, subject: "Your upcoming donation to your #{entity} charity portfolio")
+    mail(
+      to: @contribution.donor.email,
+      from: "reminders@donational.org",
+      reply_to: "support@donational.org",
+      subject: "Your upcoming donation to your #{entity} charity portfolio"
+    )
   end
 end
