2ae39c8e099b5b49c14b2f2fa281a10df0324dd2
Alejandro Monetti <pepo.1393@hotmail.com>
2018-08-07 14:30:50 -0300

    Information when collecting payment method data

--- app/commands/payments/update_customer_card.rb
+++ app/commands/payments/update_customer_card.rb
@@ -13,9 +13,15 @@ module Payments
         customer_id,
         { source: payment_token }
       )
+      
       OpenStruct.new(
         name_on_card: response[:sources][:data][0][:name],
-        last4: response[:sources][:data][0][:last4]
+        last4: response[:sources][:data][0][:last4],
+        billing_address: response[:sources][:data][0][:address_line1],
+        address_city: response[:sources][:data][0][:address_city],
+        address_state: response[:sources][:data][0][:address_state],
+        address_country: response[:sources][:data][0][:address_country],
+        address_zip_code: response[:sources][:data][0][:address_zip]
       )
     rescue Stripe::InvalidRequestError, Stripe::StripeError => e
       add_error(:customer, :stripe_error, e.message)
