0e87ce760b9a5261d866a7789164edadbbca54d2
Ian Yamey <ian@donational.org>
2018-08-26 22:15:55 -0400

    Debounce charity search

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,25 +1,26 @@
 import { Controller } from "stimulus"
+import { debounce } from "underscore"
 
 export default class extends Controller {
-  static targets = [ "text", "results" ]
+  static targets = [ "query", "results" ]
 
-  pressKey(event) {
-    if (event.keyCode == 13) {
-      event.preventDefault();
-    } else {
-      var results = this.resultsTarget;
-      $.ajax({
-        type: 'GET',
-        dataType: 'html',
-        url: event.currentTarget.dataset.autocompleteUrl,
-        data: {
-          'name': this.textTarget.value + event.key,
-          'from': event.currentTarget.dataset.autocompleteFrom
-        },
-        success: function(data) {
-          results.innerHTML = data;
-        }
-      })
-    }
+  initialize() {
+    this.search = debounce(this.search, 100)
+  }
+
+  search(event) {
+    const resultsTarget = this.resultsTarget;
+    $.ajax({
+      type: 'GET',
+      dataType: 'html',
+      url: this.queryTarget.dataset.autocompleteUrl,
+      data: {
+        'name': this.queryTarget.value,
+        'from': this.queryTarget.dataset.autocompleteFrom
+      },
+      success: function(response) {
+        resultsTarget.innerHTML = response;
+      }
+    })
   }
 }
