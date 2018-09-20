2d6fc1bc116c8dcf3d70703ddfcb37744bcd96fd
Alejandro Monetti <pepo.1393@hotmail.com>
2018-09-20 11:55:03 -0300

    Changed payment method mailer to use partner email banner

--- app/mailers/payment_methods_mailer.rb
+++ app/mailers/payment_methods_mailer.rb
@@ -1,15 +1,16 @@
 class PaymentMethodsMailer < ApplicationMailer
-  def send_payment_failed(contribution, payment_method, partner_name)
+  def send_payment_failed(contribution, payment_method, partner)
     @contribution = contribution
     @payment_method = payment_method
-    @partner_name = partner_name
+    @partner_name = partner.try(:name) || "Donational.org"
+    @partner = partner
 
     Time.use_zone(contribution.donor.time_zone) do
       mail(
         to: @contribution.donor.email,
         from: "Donational Payments <help@donational.org>",
         reply_to: "support@donational.org",
-        subject: "Your Donation to your #{partner_name} charity portfolio could not be processed"
+        subject: "Your Donation to your #{@partner_name} charity portfolio could not be processed"
       )
     end
   end
