d8d10afa3d039c76138937745e49b016567a398b
Alejandro Monetti <amonetti@effectussoftware.com>
2019-07-10 16:29:03 -0300

    Fixed Contribution Tests

--- spec/acceptance/contributions_spec.rb
+++ spec/acceptance/contributions_spec.rb
@@ -10,147 +10,181 @@ resource 'Contributions' do
   let(:api_key)       { partner.api_key }
 
   # Headers which should be included in the request
-  header 'Content-Type', 'application/json'
-  header 'X-Api-Key', :api_key
+  header 'Content-Type',  'application/json'
+  header 'X-Api-Key',     :api_key
 
-  # POST api/v1/donors/
-  post '/api/v1/organizations/:id/contributions' do
-    let(:id)  { organization.ein }
+  # POST api/v1/contributions
+  post '/api/v1/contributions' do
     # Request parameters
-    parameter :donor_id, 'Donor who makes the contribution', type: :string, required: true
-    parameter :amount, 'Contribution amount in cents (USD)', type: :integer, required: true
+    parameter :donor_id,          'Donor who makes the contribution',                       type: :string,  required: true
+    parameter :amount_cents,      'Contribution amount in cents (USD)',                     type: :integer, required: true
+    parameter :currency,          'Contribution currency. Currently only USD is supported', type: :string,  required: true
+    parameter :organization_ein,  'The tax id of the recipient charitable organization',    type: :string,  required: false
+    parameter :portfolio_id,      'The id of the charitable Portfolio to contribute to',    type: :string,  required: false
+
+    explanation 'Contributions must be created with either an organization_ein or a portfolio_id.'
 
     context '200' do
-      let(:params) do
-        {
-          contribution: {
-            donor_id: donor.id,
-            amount: 20
-          }
-        }.to_json
+      context 'Single organization' do
+        let(:params) do
+          {
+            contribution: {
+              donor_id:         donor.id,
+              amount_cents:     200,
+              currency:         'USD',
+              organization_ein: organization.ein
+            }
+          }.to_json
+        end
+
+        example 'Successful request: Make a contribution to a single organization' do
+          do_request
+
+          expect(status).to eq(200)
+          response = JSON.parse(response_body)
+          expect(response['contribution']['id']).to eq(contribution.id)
+          expect(response['contribution']['portfolio_id']).to eq(contribution.portfolio_id)
+          expect(response['contribution']['donor_id']).to eq(contribution.donor_id)
+          expect(response['contribution']['amount_cents']).to eq(contribution.amount_cents)
+        end
       end
 
-      example 'Successful request: Make a contribution to a single organization' do
-        do_request
+      context 'Portfolio' do
+        let(:params) do
+          {
+            contribution: {
+              donor_id:     donor.id,
+              amount_cents: 200,
+              currency:     'USD',
+              portfolio_id: portfolio.id
+            }
+          }.to_json
+        end
 
-        expect(status).to eq(200)
-        response = JSON.parse(response_body)
-        expect(response['contribution']['id']).to eq(contribution.id)
-        expect(response['contribution']['portfolio_id']).to eq(contribution.portfolio_id)
-        expect(response['contribution']['donor_id']).to eq(contribution.donor_id)
-        expect(response['contribution']['amount']).to eq(contribution.amount_dollars)
+        example 'Successful request: Make a contribution to a portfolio' do
+          do_request
+
+          expect(status).to eq(200)
+          response = JSON.parse(response_body)
+          expect(response['contribution']['id']).to eq(contribution.id)
+          expect(response['contribution']['portfolio_id']).to eq(contribution.portfolio_id)
+          expect(response['contribution']['donor_id']).to eq(contribution.donor_id)
+          expect(response['contribution']['amount_cents']).to eq(contribution.amount_cents)
+        end
       end
     end
 
     context '422' do
-      context 'Invalid paramter: donor_id' do
+      context 'Single organization' do
+        context 'Invalid parameter: donor_id' do
+          let(:params) do
+            {
+              contribution: {
+                donor_id:         '',
+                amount_cents:     200,
+                currency:         'USD',
+                organization_ein: organization.ein
+              }
+            }.to_json
+          end
+
+          example 'Invalid request: Make a contribution to a single organization without donor' do
+            explanation 'Invalid parameter: donor_id'
+            do_request
+
+            expect(status).to eq(422)
+            response = JSON.parse(response_body)
+            expect(response['errors'][0]).to eq("Suggested By can't be nil")
+          end
+        end
+
+        context 'Invalid parameter: amount' do
+          let(:params) do
+            {
+              contribution: {
+                donor_id:         donor.id,
+                amount_cents:     50,
+                currency:         'USD',
+                organization_ein: organization.ein
+              }
+            }.to_json
+          end
+
+          example 'Invalid request: Make a contribution to a single organization without amount' do
+            explanation 'Invalid parameter: amount'
+            do_request
+
+            expect(status).to eq(422)
+            response = JSON.parse(response_body)
+            expect(response['errors'][0]).to eq("Amount Cents is too small")
+          end
+        end
+      end
+
+      context 'Portfolio' do
+        context 'Invalid parameter: donor_id' do
+          let(:params) do
+            {
+              contribution: {
+                donor_id:     '',
+                amount_cents: 200,
+                currency:     'USD',
+                portfolio_id: portfolio.id
+              }
+            }.to_json
+          end
+
+          example 'Invalid request: Make a contribution to a portfolio without donor' do
+            explanation 'Invalid parameter: donor_id'
+            do_request
+
+            expect(status).to eq(422)
+            response = JSON.parse(response_body)
+            expect(response['errors'][0]).to eq("Donor can't be nil")
+          end
+        end
+
+        context 'Invalid parameter: amount' do
+          let(:params) do
+            {
+              contribution: {
+                donor_id:     donor.id,
+                amount_cents: 0,
+                currency:     'USD',
+                portfolio_id: portfolio.id
+              }
+            }.to_json
+          end
+
+          example 'Invalid request: Make a contribution to a portfolio without amount' do
+            explanation 'Invalid parameter: amount'
+            do_request
+
+            expect(status).to eq(422)
+            response = JSON.parse(response_body)
+            expect(response['errors'][0]).to eq("Amount Cents is too small")
+          end
+        end
+      end
+
+      context 'Invalid parameters: missing organization_ein and portfolio_id' do
         let(:params) do
           {
             contribution: {
-              donor_id: '',
-              amount: 20,
+              donor_id:         donor.id,
+              amount_cents:     200,
+              currency:         'USD'
             }
           }.to_json
         end
 
-        example 'Invalid request: Make a contribution to a single organization without donor' do
-          explanation 'Invalid parameter: donor_id'
+        example 'Invalid request: Make a contribution without organization_ein or portfolio_id' do
+          explanation 'Invalid parameters: missing organization_ein and portfolio_id'
           do_request
 
           expect(status).to eq(422)
           response = JSON.parse(response_body)
-          expect(response['errors'][0]).to eq("Donor can't be nil")
-        end
-      end
-
-      context 'Invalid parameter: amount' do
-        let(:params) do
-          {
-            contribution: {
-              donor_id: donor.id,
-              amount: 0,
-            }
-          }.to_json
-        end
-
-        example 'Invalid request: Make a contribution to a single organization without amount' do
-          explanation 'Invalid parameter: amount'
-          do_request
-
-          expect(status).to eq(422)
-          response = JSON.parse(response_body)
-          expect(response['errors'][0]).to eq("Amount Cents is too small")
-        end
-      end
-    end
-  end
-
-  post '/api/v1/portfolios/:id/contributions' do
-    let(:id)  { portfolio.id }
-    # Request parameters
-    parameter :donor_id, 'Donor who makes the contribution', type: :string, required: true
-    parameter :amount, 'Contribution amount in USD', type: :string, required: true
-
-    context '200' do
-      let(:params) do
-        {
-          contribution: {
-            donor_id: donor.id,
-            amount: 20
-          }
-        }.to_json
-      end
-
-      example 'Successful request: Make a contribution to a portfolio' do
-        do_request
-
-        expect(status).to eq(200)
-        response = JSON.parse(response_body)
-        expect(response['contribution']['id']).to eq(contribution.id)
-        expect(response['contribution']['portfolio_id']).to eq(contribution.portfolio_id)
-        expect(response['contribution']['donor_id']).to eq(contribution.donor_id)
-        expect(response['contribution']['amount']).to eq(contribution.amount_dollars)
-      end
-    end
-
-    context '422' do
-      context 'Invalid paramter: donor_id' do
-        let(:params) do
-          {
-            contribution: {
-              donor_id: '',
-              amount: 20,
-            }
-          }.to_json
-        end
-
-        example 'Invalid request: Make a contribution to a portfolio without donor' do
-          explanation 'Invalid parameter: donor_id'
-          do_request
-
-          expect(status).to eq(422)
-          response = JSON.parse(response_body)
-          expect(response['errors'][0]).to eq("Donor can't be nil")
-        end
-      end
-
-      context 'Invalid parameter: amount' do
-        let(:params) do
-          {
-            contribution: {
-              donor_id: donor.id,
-              amount: 0,
-            }
-          }.to_json
-        end
-
-        example 'Invalid request: Make a contribution to a portfolio without amount' do
-          explanation 'Invalid parameter: amount'
-          do_request
-
-          expect(status).to eq(422)
-          response = JSON.parse(response_body)
-          expect(response['errors'][0]).to eq("Amount Cents is too small")
+          expect(response['errors']).to eq('Either Organization ein or Portfolio id should be present')
         end
       end
     end
