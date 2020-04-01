410f301252b9392d301b0940796cab8b853901d0
Ian Yamey <ian@donational.org>
2020-04-01 17:32:14 -0400

    Fix embedded donation-box (#232)

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CampaignsController < ApplicationController
   protect_from_forgery unless: -> { request.format.js? }
 
-  before_action :ensure_donor_has_permission!, except: [:show, :donation_box]
+  before_action :ensure_donor_has_permission!, except: %i[show donation_box]
 
-  layout "embeddable", only: :donation_box
+  layout 'embeddable', only: :donation_box
 
   def index
     @view_model = OpenStruct.new(partner: partner)
@@ -36,9 +38,9 @@ class CampaignsController < ApplicationController
 
     respond_to do |format|
       format.js
-      format.html {
+      format.html do
         allow_iframe_embedding_on_partner_website!
-      }
+      end
     end
   end
 
@@ -46,7 +48,7 @@ class CampaignsController < ApplicationController
     @view_model = OpenStruct.new(
       partner: partner,
       campaign: Campaign.new,
-      currency: partner_currency,
+      currency: partner_currency
     )
   end
 
@@ -55,8 +57,8 @@ class CampaignsController < ApplicationController
       partner: partner,
       campaign: campaign_by_id,
       banner_image: campaign_by_id.banner_image,
-      default_contribution_amounts: campaign_by_id.default_contribution_amounts.join(", "),
-      currency: partner_currency,
+      default_contribution_amounts: campaign_by_id.default_contribution_amounts.join(', '),
+      currency: partner_currency
     )
   end
 
@@ -72,7 +74,7 @@ class CampaignsController < ApplicationController
       allow_one_time_contributions: params[:allow_one_time_contributions]
     )
 
-    flash[:success] = "Campaign created successfully." if command.success?
+    flash[:success] = 'Campaign created successfully.' if command.success?
     flash[:error] = command.errors.message_list.join('. ') unless command.success?
     redirect_to partner_campaigns_path(partner)
   end
@@ -89,7 +91,7 @@ class CampaignsController < ApplicationController
       allow_one_time_contributions: params[:allow_one_time_contributions]
     )
 
-    flash[:success] = "Campaign updated successfully." if command.success?
+    flash[:success] = 'Campaign updated successfully.' if command.success?
     flash[:error] = command.errors.message_list.join('. ') unless command.success?
     redirect_to edit_partner_campaign_path(partner, campaign_by_id)
   end
@@ -98,21 +100,32 @@ class CampaignsController < ApplicationController
     not_found unless campaign
 
     @view_model = OpenStruct.new(
+      partner_name: partner.name,
+      partner_description: partner.description,
+      partner_website_url: partner.website_url,
+      partner_logo: partner.logo,
+      banner_image: campaign.banner_image,
+      campaign_title: campaign.title,
       campaign_slug: campaign.slug,
+      campaign_description: campaign.description,
       contribution_amount_help_text: campaign.contribution_amount_help_text,
       donation_frequencies: campaign.allowable_donation_frequencies,
       default_contribution_amounts: campaign.default_contribution_amounts,
       campaign_contributions_path: campaign_contributions_path(campaign.slug),
       new_campaign_contribution: new_campaign_contribution,
       managed_portfolios: managed_portfolios,
-      donor_questions: partner.donor_questions
+      donor_questions: partner.donor_questions,
+      default_operating_costs_donation_percentages: partner.default_operating_costs_donation_percentages,
+      partner_operating_costs_text: partner.operating_costs_text,
+      partner_accepts_operating_costs_donations?: partner.accepts_operating_costs_donations?,
+      currency: partner_currency
     )
 
     respond_to do |format|
       format.js
-      format.html {
+      format.html do
         allow_iframe_embedding_on_partner_website!
-      }
+      end
     end
   end
 
@@ -126,9 +139,9 @@ class CampaignsController < ApplicationController
   end
 
   def allow_iframe_embedding_on_partner_website!
-    response.headers["X-Content-Security-Policy"] = "frame-ancestors #{partner.website_url}"
-    response.headers["Content-Security-Policy"] = "frame-ancestors #{partner.website_url}"
-    response.headers.delete "X-Frame-Options"
+    response.headers['X-Content-Security-Policy'] = "frame-ancestors #{partner.website_url}"
+    response.headers['Content-Security-Policy'] = "frame-ancestors #{partner.website_url}"
+    response.headers.delete 'X-Frame-Options'
   end
 
   def new_campaign_contribution
@@ -161,7 +174,7 @@ class CampaignsController < ApplicationController
   end
 
   def default_contribution_amounts
-    params[:default_contribution_amounts].split(',').map { |amount| amount.to_i }
+    params[:default_contribution_amounts].split(',').map(&:to_i)
   end
 
   def managed_portfolios
