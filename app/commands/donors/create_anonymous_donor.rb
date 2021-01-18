7c8844a0d03ebf3353f0f1c950d753ec81feb13b
Alejandro Monetti <amonetti@effectussoftware.com>
2021-01-18 14:56:51 -0300

    Partner Affiliation should be created for Donational-only donors (#318)

--- app/commands/donors/create_anonymous_donor.rb
+++ app/commands/donors/create_anonymous_donor.rb
@@ -5,7 +5,18 @@ module Donors
     end
 
     def execute
-      Donor.create!(id: donor_id)
+      donor = Donor.new(id: donor_id)
+      donor.save!
+
+      Partners::AffiliateDonorWithPartner.run(donor: donor, partner: partner)
+
+      donor
+    end
+
+    private
+
+    def partner
+      Partner.find_by(name: Partner::DEFAULT_PARTNER_NAME)
     end
   end
 end
