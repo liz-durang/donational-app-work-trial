9e47e146fc6f7e2980db002504d699dc79bfa7ce
Ian Yamey <ian@donational.org>
2025-05-11 22:39:51 -0400

    Add default to 20250312043116_add_timestamps_to_payment_method.rb (#530)

--- db/migrate/20250312043116_add_timestamps_to_payment_method.rb
+++ db/migrate/20250312043116_add_timestamps_to_payment_method.rb
@@ -1,5 +1,5 @@
 class AddTimestampsToPaymentMethod < ActiveRecord::Migration[7.0]
   def change
-    add_timestamps :payment_methods
+    add_timestamps :payment_methods, default: Time.zone.now
   end
 end
